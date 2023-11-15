fbbdc30d9a14db0df01aa014993166e1389566a4
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-11-15 17:47:21 +0100

    tweak the code to make it run on the cluster

--- AutoFormer/supernet_train.py
+++ AutoFormer/supernet_train.py
@@ -148,7 +148,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data-path', default='./data/imagenet/', type=str,
                         help='dataset path')
-    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR', 'IMNET', 'INAT', 'INAT19'],
+    parser.add_argument('--data-set', default='IMNET', choices=['CIFAR10', 'CIFAR100', 'IMNET', 'INAT', 'INAT19'],
                         type=str, help='Image Net dataset path')
     parser.add_argument('--inat-category', default='name',
                         choices=['kingdom', 'phylum', 'class', 'order', 'supercategory', 'family', 'genus', 'name'],
