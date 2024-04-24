e0924c28b80bb3ebabba54aa497018208b6c2f64
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-04-24 17:28:34 +0200

    Revert "remove explicit setting of number of workers in DataLoaders"
    This reverts commit b78eb27dc928d38a1ac65dc3150ec76c1ba69c7b.

--- AutoFormer/supernet_darts_train.py
+++ AutoFormer/supernet_darts_train.py
@@ -213,12 +213,12 @@ def main():
   train_queue = torch.utils.data.DataLoader(
       train_data, batch_size=args.batch_size,
       sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[:split]),
-      pin_memory=True)
+      pin_memory=True, num_workers=2)
 
   valid_queue = torch.utils.data.DataLoader(
       train_data, batch_size=args.batch_size,
       sampler=torch.utils.data.sampler.SubsetRandomSampler(indices[split:num_train]),
-      pin_memory=True)
+      pin_memory=True, num_workers=2)
 
 
   optimizer = create_optimizer(args, model)
