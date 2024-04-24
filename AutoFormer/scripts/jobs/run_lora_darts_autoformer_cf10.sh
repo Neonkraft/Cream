064c58a10361e65a01932a0c17a2347090480407
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-04-24 17:09:54 +0200

    fix issue with seeding and number of cores

--- AutoFormer/scripts/jobs/run_lora_darts_autoformer_cf10.sh
+++ AutoFormer/scripts/jobs/run_lora_darts_autoformer_cf10.sh
@@ -3,7 +3,8 @@
 #SBATCH -o logs/%x.%N.%j.out # STDOUT
 #SBATCH -e logs/%x.%N.%j.err # STDERR
 #SBATCH -a 1-3 # array size
-#SBATCH -J LoRA-DARTS-AutoFormer-CF10 # sets the job name.
+#SBATCH -c 8 # number of cores per task
+#SBATCH -J LoRA-DARTS-AutoFormer-CF50-E500-W50-BS96 # sets the job name.
 echo "Workingdir: $PWD";
 echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CUPS_PER_NODE gpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
@@ -14,7 +15,7 @@ start=`date +%s`
 source ~/.bashrc
 conda activate autoformer
 
-python supernet_darts_train.py --epochs 100 --warmup_epochs 50
+python supernet_darts_train.py --epochs 500 --warmup_epochs 50 --batch_size 96 --seed $SLURM_ARRAY_TASK_ID
 
 end=`date +%s`
 runtime=$((end-start))
